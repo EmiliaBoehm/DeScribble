cb01f37971ad7ddae72a9f40bd657ae3a2db07ad
Jörg Volbers <joerg@joergvolbers.de>
2023-11-13 11:48:39 +0100

    Finalize script

--- src/simplemodel.py
+++ src/simplemodel.py
@@ -1,7 +1,6 @@
 """Load the pretrained TrOCR model from huggingface.com and run it with an example image."""
 
 import argparse
-import sys
 import os
 from PIL import Image
 from transformers import TrOCRProcessor, VisionEncoderDecoderModel
@@ -58,42 +57,21 @@ def predict(image_tensor: torch.Tensor,
 def get_ArgumentParser() -> argparse.ArgumentParser:
     """Return an ArgumentParser object for this script."""
     parser = argparse.ArgumentParser(
-        description="A simple text wrapper."
+        description="Scan the image and print the results"
     )
-    # parser.add_argument("inputfile")
-    # parser.add_argument("outputfile")
+    parser.add_argument("inputfile")
+#parser.add_argument("outputfile")
     return parser
-
-
-def wrap_text(text: str) -> str:
-    """Wrap some text around TEXT."""
-    return "---ADDED LINES---\n" + \
-           text + \
-           "---END OF ADDED LINES---\n"
-
-
-def wrap_file(file) -> str:
-    """Read FILE and return it wrapped in some text."""
-    try:
-        with open(file, 'r') as f:
-            file_content = f.read()   # read the complete file
-    except (FileNotFoundError, IsADirectoryError) as err:
-        print(f"{sys.argv[0]}: {file}: {err.strerror}", file=sys.stderr)
-    return wrap_text(file_content)
-
-
-def write_file(file, text: str) -> None:
-    """Store TEXT in FILE."""
-    with open(file, 'w') as f:
-        f.write(text)
 
 
 def main() -> None:
     """Parse arguments and dispatch."""
     args = get_ArgumentParser().parse_args()
-    # print(f"Input file: {args.inputfile}")
-    # print(f"Output file: {args.outputfile}")
-#    write_file(args.outputfile, wrap_file(args.inputfile))
+    image = get_image(args.inputfile)
+    model = get_model("microsoft/trocr-base-handwritten", "simplemodel")
+    processor = get_processor("microsoft/trocr-base-handwritten", "simplemodel")
+    tensor = format_image(image, processor)
+    return predict(tensor, model, processor)
 
 
 if __name__ == "__main__":
